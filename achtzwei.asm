45aa35475c9c158003c291b33b526455fe1322be
astron-1342 <astron-1342@users.noreply.github.com>
2015-04-02 16:35:35 +0200

    Update achtzwei.asm

--- achtzwei.asm
+++ achtzwei.asm
@@ -4,29 +4,33 @@ jmp START
 CSEG AT 100h
 START:
 
-mov r3,#221
-mov a, r3
-mov b, #100
+mov r3,#221 ; Festlegen der zu zerlegenden Zahl
+
+mov a, r3   ; Durch Hundert und in r0 speichern
+mov b, #100 
 div ab
 mov r0, a
-mov b, #100
+
+mov b, #100 ; Abziehen der Hunderter vom Anfangswert
 mul ab
 mov b,a
 mov a, r3
 subb a, b
 
-mov r4, a
+mov r4, a   ; Division durch Zehner und speichern in r1
 mov b, #10
 div ab
 mov r1, a
-mov b, #10
+
+mov b, #10  ; Zehnerziffer in b
 mov a, r1
 mul ab
 mov b,a
-mov a, r4
-subb a, b
 
-mov r2, a
+mov a, r4 ; Anfangswert ohne Hunderter in a
+subb a, b ; Abziehen der Zehner
+
+mov r2, a ; Übrig bleibt die Einserziffer, speichern in r2
 
 
-END
+END ; ENDE
