c44a9ee15fffeae7e355f7fe77ce9efec59f71dd
astron-1342 <astron-1342@users.noreply.github.com>
2015-04-02 15:31:52 +0200

    Create a5_4.c

---
+++ a5_4.c
@@ -0,0 +1,20 @@
+#include <XC888CLM.h>
+
+sbit TASTER_1 = 0xA0;
+
+void main(void)
+{  char zaehler=0;
+	P3_DIR=0xFF;
+	while(1)
+	{
+		if (TASTER_1)
+			{
+			while(TASTER_1 != 0){}	
+			zaehler ++;
+			}
+			
+		if((zaehler%2)==0)
+			{P3_DATA=0xF0;}
+		else{P3_DATA=0x00;}
+	}
+  }
