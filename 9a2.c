61807a2bb64c9aab62ab070265846f1544e5088a
astron-1342 <astron-1342@users.noreply.github.com>
2015-04-26 19:44:28 +0200

    Update 9a2.c

--- 9a2.c
+++ 9a2.c
@@ -10,3 +10,23 @@ unsigned char zaehler_ueberlauf = 0;
 P3_DIR=0xFF;
 P3_DATA=0;
 
+TMOD = 1;
+TCON = 0x10;
+while (1)
+{
+ while(TFO == 0){;}
+ TF0= 0;
+ TL0 = 0xA0;
+ TH0 = 0x15;
+ zaehler_ueberlauf ++;
+ 
+ if(zaehler_ueberlauf == 200)
+ {
+   zaehler_ueberlauf = 0;
+   P3_DATA ++;
+ }
+ 
+  
+}
+
+}
